7482d982d46e3c2ea0421104db254b08388fc5c3
Anderson F. Silva <www.anderson-ferdnand07@hotmail.com>
2023-07-03 21:04:05 -0300

    Exercicio finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import './styles'
+import { Container, EstiloGlobal } from './styles'
 
 function App() {
   return (
     <>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
-        <ListaVagas />
-      </div>
+      <Container>
+      <ListaVagas />
+      </Container>
     </>
   )
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,12 @@
-import styles from './Hero.module.css'
+import { Container } from '../../styles'
+import { HeroForm } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <HeroForm>
+    <Container>
+      <h2>As melhores vagas para tecnologia, design e artes visuais.</h2>
+    </Container>
+  </HeroForm>
 )
 
 export default Hero
